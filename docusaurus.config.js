7e0e286771cf1bb38ccb8d19edcd552d5ed5eb8a
q-org <382362036@qq.com>
2023-06-23 17:49:58 +0800

    https://qorg.xyz/

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -70,7 +70,7 @@ const isBranchDeploy =
 const config = {
   title: '微课程',
   tagline: '微课程很酷',
-  url: 1 ? 'https://qio.wiki' : 'https://q-org.netlify.app/',
+  url: 0 ? 'https://qio.wiki' : 'https://q-org.netlify.app/',
   // url: 'https://zingy-tiramisu-028c02.netlify.app/',
   baseUrl: '/',
   stylesheets: [
